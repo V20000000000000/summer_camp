48456e72e887a600e0f90f55b45ab1484d84997a
V4000 <johndoe@example.com>
2025-02-18 17:03:42 +0800

    Add five students into linked list

---
+++ double_linked_list/linked_list_task.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "list.h"
+
+// struct student
+struct student {
+    struct list_head list;
+    int age;
+    char name[30];
+    int score;
+};
+
+
+/*
+ * Add several nodes into the doubly linked list.
+ * Then traverse all the nodes.
+ */
+
+int main()
+{
+    LIST_HEAD(head);
+
+    // add five student into linked list
+
+    printf("============================================================\n");
+    printf("Add five students into linked list.\n");
+    printf("============================================================\n");
+
+    struct student student1 = { .age = 21, .name = "Calvin1", .score = 50 };
+    list_add(&student1.list, &head);
+
+    struct student student2 = { .age = 22, .name = "Calvin2", .score = 60 };
+    list_add(&student2.list, &head);
+
+    struct student student3 = { .age = 23, .name = "Calvin3", .score = 70 };
+    list_add(&student3.list, &head);
+
+    struct student student4 = { .age = 24, .name = "Calvin4", .score = 80 };
+    list_add(&student4.list, &head);
+
+    struct student student5 = { .age = 25, .name = "Calvin5", .score = 90 };
+    list_add(&student5.list, &head);
+
+
+
+    // traversing the list using list_for_each
+    printf("traversing the list using list_for_each\n");
+    printf("list_for_each()\n");
+
+    struct list_head *it = NULL;
+    list_for_each(it, &head) {
+        printf("Student Name: %s Age: %d Score: %d\n", list_entry(it, struct student, list)->name, list_entry(it, struct student, list)->age, list_entry(it, struct student, list)->score);
+    }
+    printf("\n");
+
+    return 0;
+}
