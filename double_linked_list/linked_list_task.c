9f973a67ecde25a8571469887c919647fcffd12b
V4000 <johndoe@example.com>
2025-02-18 17:28:28 +0800

    removing specific age update

--- double_linked_list/linked_list_task.c
+++ double_linked_list/linked_list_task.c
@@ -19,34 +19,57 @@ int main()
 {
     LIST_HEAD(head);
 
-    // add five student into linked list
+    /*
+        ADD FIVE STUDENT INTO LINKED LIST AND PRINT TO TERMINAL
+    */
 
+    // add five student into linked list
     printf("============================================================\n");
     printf("Add five students into linked list.\n");
     printf("============================================================\n");
 
-    struct student student1 = { .age = 21, .name = "Calvin1", .score = 50 };
-    list_add(&student1.list, &head);
-
-    struct student student2 = { .age = 22, .name = "Calvin2", .score = 60 };
-    list_add(&student2.list, &head);
-
-    struct student student3 = { .age = 23, .name = "Calvin3", .score = 70 };
-    list_add(&student3.list, &head);
-
-    struct student student4 = { .age = 24, .name = "Calvin4", .score = 80 };
-    list_add(&student4.list, &head);
-
     struct student student5 = { .age = 25, .name = "Calvin5", .score = 90 };
     list_add(&student5.list, &head);
-
-
+    struct student student4 = { .age = 24, .name = "Calvin4", .score = 80 };
+    list_add(&student4.list, &head);
+    struct student student3 = { .age = 23, .name = "Calvin3", .score = 70 };
+    list_add(&student3.list, &head);
+    struct student student2 = { .age = 22, .name = "Calvin2", .score = 60 };
+    list_add(&student2.list, &head);
+    struct student student1 = { .age = 21, .name = "Calvin1", .score = 50 };
+    list_add(&student1.list, &head);
 
     // traversing the list using list_for_each
     printf("traversing the list using list_for_each\n");
     printf("list_for_each()\n");
 
     struct list_head *it = NULL;
+    list_for_each(it, &head) {
+        printf("Student Name: %s Age: %d Score: %d\n", list_entry(it, struct student, list)->name, list_entry(it, struct student, list)->age, list_entry(it, struct student, list)->score);
+    }
+    printf("\n\n");
+
+
+    /*
+        Removing student's age is 22 and print to terminal
+    */
+
+    printf("=====================list_del<pos>====================\n");
+    printf("Removing student's age is 22\n");
+    printf("Redisplay list after removing\n");
+
+    // Removing student's age is 22
+    list_for_each(it, &head) {
+        while(list_entry(it, struct student, list)->age == 22)
+        {
+            list_del(it);
+        }
+    }
+
+    // traversing the list using list_for_each
+    printf("traversing the list using list_for_each\n");
+    printf("list_for_each()\n");
+
     list_for_each(it, &head) {
         printf("Student Name: %s Age: %d Score: %d\n", list_entry(it, struct student, list)->name, list_entry(it, struct student, list)->age, list_entry(it, struct student, list)->score);
     }
